90cd1620902d09d814b08e85b1dd0a522df29ac5
adbaga <bagaskaraiqbal7a@gmail.com>
2021-07-26 12:44:27 +0300

    initial foundation

---
+++ src/components/User.js
@@ -0,0 +1,153 @@
+import React, { useState, useEffect } from "react";
+import { useDispatch } from "react-redux";
+import { updateUser, deleteUser } from "../actions/users";
+import UserDataService from "../services/UserService";
+
+
+const User = (props) => {
+  const initialUserState = {
+    id: null,
+    userName: "",
+    firstName: "",
+    lastName: "", 
+    email: "",
+  };
+  const [currentUser, setCurrentUser] = useState(initialUserState);
+  const [message, setMessage] = useState("");
+
+  const dispatch = useDispatch();
+
+  const getUser = id => {
+    UserDataService.get(id)
+      .then(response => {
+        setCurrentUser(response.data);
+        console.log(response.data);
+      })
+      .catch(e => {
+        console.log(e);
+      });
+  };
+
+  useEffect(() => {
+    getUser(props.match.params.id);
+  }, [props.match.params.id]);
+
+  const handleInputChange = event => {
+    const { name, value } = event.target;
+    setCurrentUser({ ...currentUser, [name]: value });
+  };
+
+  const updateStatus = status => {
+    const data = {
+      id: currentUser.id,
+      userName: currentUser.userName,
+      firstName: currentUser.firstName,
+      lastName: currentUser.lastName, 
+      email: currentUser.email,
+    };
+
+    dispatch(updateUser(currentUser.id, data))
+      .then(response => {
+        console.log(response);
+
+        setCurrentUser({ ...currentUser, published: status });
+        setMessage("The status was updated successfully!");
+      })
+      .catch(e => {
+        console.log(e);
+      });
+  };
+
+  const updateContent = () => {
+    dispatch(updateUser(currentUser.id, currentUser))
+      .then(response => {
+        console.log(response);
+
+        setMessage("The user was updated successfully!");
+      })
+      .catch(e => {
+        console.log(e);
+      });
+  };
+
+  const removeUser = () => {
+    dispatch(deleteUser(currentUser.id))
+      .then(() => {
+        props.history.push("/users");
+      })
+      .catch(e => {
+        console.log(e);
+      });
+  };
+
+  return (
+    <div>
+    {currentUser ? (
+      <div className="edit-form">
+        <h4>User</h4>
+        <form>
+          <div className="form-group">
+            <label htmlFor="userName">User Name</label>
+            <input
+              type="text"
+              className="form-control"
+              id="title"
+              name="title"
+              value={currentUser.userName}
+              onChange={handleInputChange}
+            />
+          </div>
+          <div className="form-group">
+            <label htmlFor="firstName">First Name</label>
+            <input
+              type="text"
+              className="form-control"
+              id="firstName"
+              name="firstName"
+              value={currentUser.firstName}
+              onChange={handleInputChange}
+            />
+          </div>
+
+          <div className="form-group">
+            <label htmlFor="lasstName">Last Name</label>
+            <input
+              type="text"
+              className="form-control"
+              id="lastName"
+              name="lastName"
+              value={currentUser.lastName}
+              onChange={handleInputChange}
+            />
+          </div>
+
+
+
+        </form>
+
+
+
+        <button className="badge badge-danger mr-2" onClick={removeUser}>
+          Delete
+        </button>
+
+        <button
+          type="submit"
+          className="badge badge-success"
+          onClick={updateContent}
+        >
+          Update
+        </button>
+        <p>{message}</p>
+      </div>
+    ) : (
+      <div>
+        <br />
+        <p>Please click on a User...</p>
+      </div>
+    )}
+  </div>
+  );
+};
+
+export default User;
---
+++ src/components/UsersList.js
@@ -0,0 +1,147 @@
+import React, { useState, useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { Link } from "react-router-dom";
+import {
+  retrieveUsers,
+  findUserByUserName,
+  deleteAllUsers,
+} from "../actions/users";
+
+const UsersList = () => {
+  const [currentUser, setCurrentUser] = useState(null);
+  const [currentIndex, setCurrentIndex] = useState(-1);
+  const [searchUserName, setSearchUserName] = useState("");
+
+  const users = useSelector(state => state.users);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(retrieveUsers());
+  }, []);
+
+  const onChangeSearchUserName = e => {
+    const searchUserName = e.target.value;
+    setSearchUserName(searchUserName);
+  };
+
+  const refreshData = () => {
+    setCurrentUser(null);
+    setCurrentIndex(-1);
+  };
+
+  const setActiveUser = (user, index) => {
+    setCurrentUser(user);
+    setCurrentIndex(index);
+  };
+
+  const removeAllUsers = () => {
+    dispatch(deleteAllUsers())
+      .then(response => {
+        console.log(response);
+        refreshData();
+      })
+      .catch(e => {
+        console.log(e);
+      });
+  };
+
+  const findUserByUserName = () => {
+    refreshData();
+    dispatch(findUserByUserName(searchUserName));
+  };
+
+  return (
+    <div className="list row">
+      <div className="col-md-8">
+        <div className="input-group mb-3">
+          <input
+            type="text"
+            className="form-control"
+            placeholder="Search by User Name"
+            value={searchUserName}
+            onChange={onChangeSearchUserName}
+          />
+          <div className="input-group-append">
+            <button
+              className="btn btn-outline-secondary"
+              type="button"
+              onClick={findUserByUserName}
+            >
+              Search
+            </button>
+          </div>
+        </div>
+      </div>
+      <div className="col-md-6">
+        <h4>Users List</h4>
+
+        <ul className="list-group">
+          {users &&
+            users.map((user, index) => (
+              <li
+                className={
+                  "list-group-item " + (index === currentIndex ? "active" : "")
+                }
+                onClick={() => setActiveUser(user, index)}
+                key={index}
+              >
+                {user.userName}
+              </li>
+            ))}
+        </ul>
+
+        <button
+          className="m-3 btn btn-sm btn-danger"
+          onClick={removeAllUsers}
+        >
+          Remove All
+        </button>
+      </div>
+      <div className="col-md-6">
+        {currentUser ? (
+          <div>
+            <h4>User</h4>
+            <div>
+              <label>
+                <strong>User Name:</strong>
+              </label>{" "}
+              {currentUser.userName}
+            </div>
+            <div>
+              <label>
+                <strong>First Name:</strong>
+              </label>{" "}
+              {currentUser.firstName}
+            </div>
+            <div>
+              <label>
+                <strong>Last Name:</strong>
+              </label>{" "}
+              {currentUser.lastName}
+            </div>
+            <div>
+              <label>
+                <strong>Email:</strong>
+              </label>{" "}
+              {currentUser.email}
+            </div>
+
+            <Link
+              to={"/users/" + currentUser.id}
+              className="badge badge-warning"
+            >
+              Edit
+            </Link>
+          </div>
+        ) : (
+          <div>
+            <br />
+            <p>Please click on a User...</p>
+          </div>
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default UsersList;
